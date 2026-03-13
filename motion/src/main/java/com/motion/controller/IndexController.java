8b92efb0bfd12803322bed43b15c4602ced5099c
uoehoswi3 <thdus1821@naver.com>
2026-03-13 16:03:05 +0900

    aside 추가

--- motion/src/main/java/com/motion/controller/IndexController.java
+++ motion/src/main/java/com/motion/controller/IndexController.java
@@ -1,11 +1,11 @@
-package com.motion.motion;
+package com.motion.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class motionController {
+public class IndexController {
 
     @GetMapping("/")
     public String goPage() {
