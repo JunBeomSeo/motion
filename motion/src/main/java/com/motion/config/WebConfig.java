9f9905b5c016720a36507f6764c10f691ce1a529
uoehoswi3 <thdus1821@naver.com>
2026-03-25 15:22:54 +0900

    aside 수정

---
+++ motion/src/main/java/com/motion/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.motion.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String path = System.getProperty("user.dir") + "/profiles/";
+
+        registry.addResourceHandler("/profiles/**")
+                .addResourceLocations("file:///" + path);
+    }
+}
